2a450feefb11da163805675f24a00b514923ec06
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-02-20 20:05:12 -0500

    Configure docker compost to run db and server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, (req, res) => {
-    console.log("Server is running on port 3000")
+app.listen(3000, () => {
+    console.log("Server has started on port 3000")
 })

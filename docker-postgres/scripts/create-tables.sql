3e1a743d40c13c4e1fb27117965c341837ffad1e
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-02-26 21:49:02 -0500

    Get SQL tables created on database init

--- docker-postgres/scripts/create-tables.sql
+++ docker-postgres/scripts/create-tables.sql
@@ -1,14 +1,14 @@
-CREATE TABLE [IF NOT EXISTS] categories (
+CREATE TABLE categories (
     cat_id VARCHAR PRIMARY KEY,
-    cat_name VARCHAR,
+    cat_name VARCHAR
 );
 
-CREATE TABLE [IF NOT EXISTS] transactions (
+CREATE TABLE transactions (
     id VARCHAR PRIMARY KEY,
     date TIMESTAMP,
     amount INT NOT NULL,
-    notes VARCHAR
-    cat_id VARCHAR
+    notes VARCHAR,
+    cat_id VARCHAR,
     CONSTRAINT fk_category
         FOREIGN KEY(cat_id)
             REFERENCES categories(cat_id)
